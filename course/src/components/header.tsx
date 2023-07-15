5ea11d768cb01cfc93784553bea347c9dd9e324e
GaoXiong <xiong.gao@shopee.com>
2023-07-15 23:34:13 +0800

    chore: update

--- course/src/components/header.tsx
+++ course/src/components/header.tsx
@@ -1,14 +1,53 @@
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 
 function Header() {
+  const location = useLocation();
+  const [index, setIndex] = useState(0);
+  const list = [
+    {
+      title: "专题",
+      path: '/'
+    },
+    {
+      title: "文章",
+      path: '/article'
+    },
+    {
+      title: "友链",
+      path: '/post/link'
+    },
+    {
+      title: "作者",
+      path: '/post/author'
+    }
+  ]
+
+  useEffect(() => {
+    let index = 0
+    if (window.location.href.includes("post/author")) {
+      index = 3;
+    } else if (window.location.href.includes("post/link")) {
+      index = 2;
+    } else if (window.location.href.includes("article")) {
+      index = 1;
+    }
+    setIndex(index)
+  }, [location])
+
+
   return (
     <div className="header">
       <div className="navbar g-w">
         <div>
-          <Link className="link" to="/">专题</Link>
-          <Link className="link" to="/article">文章</Link>
-          <Link className="link" to={"/post/link"}>友链</Link>
-          <Link className="link" to={"/post/author"}>作者</Link>
+          {
+            list.map((it, i) => {
+              const cls = index === i ? 'link active' : 'link';
+              return (
+                <Link className={cls} to={it.path}>{it.title}</Link>
+              )
+            })
+          }
         </div>
         <div></div>
       </div>
